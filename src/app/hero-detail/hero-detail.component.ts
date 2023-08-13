1dc3acad20468bbecee9960c7f24a75edc82a07f
Akibayashi23 <nissyokada23@gmail.com>
2023-08-13 18:22:47 +0900

    3. フィーチャーコンポーネントの作成

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  // 子に定義することで親で使えるようにする
+  @Input() hero?: Hero;
+}
