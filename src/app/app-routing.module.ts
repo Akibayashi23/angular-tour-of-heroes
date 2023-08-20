392da63591651f19d439cf3a490e23004758cbb6
Akibayashi23 <nissyokada23@gmail.com>
2023-08-20 16:40:02 +0900

    5. ナビゲーションの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+// routerLinkキーワードから呼ばれる
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // URLがlocalhost:4200/heroesのようなものである場合にHeroesComponentを表示します。
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent },
+];
 
 @NgModule({
+  // forRoot()メソッドは、ルーティングに必要なサービス・プロバイダーとディレクティブを提供し、ブラウザの現在のURLを元に最初の遷移を行います。
   imports: [RouterModule.forRoot(routes)],
+  // アプリケーション全体で利用できるようにします。
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   // 子に定義することで親で使えるようにする
-  @Input() hero?: Hero;
+  hero: Hero | undefined;
+
+  constructor(
+    // HeroDetailComponentのインスタンスへのルートに関する情報を保持します。 このコンポーネントは、URLから抽出されたルートのパラメータに関心があります。 "id"パラメータは、表示するヒーローのidです。
+    private route: ActivatedRoute,
+    // リモートサーバーからヒーローのデータを取得し、このコンポーネントはそれを使用して表示するヒーローを取得します。
+    private heroService: HeroService,
+    // ブラウザと対話するためのAngularサービスです。 これは前のビューに戻ることができるサービスです。
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    // route.snapshotは、コンポーネントが作成された直後のルート情報の静的イメージです。
+    // paramMapは、URL から抽出されたルートパラメータ値の辞書です。 "id"キーは、フェッチするヒーローのidを返します。
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
